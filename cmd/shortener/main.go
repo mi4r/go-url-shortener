f7ef52e1fd94e2d2577a30d4a587233cad590345
Tigran <aruttigran1203@yandex.ru>
2024-07-01 00:24:17 +0300

    add handlers

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,34 +2,80 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"math/rand"
 	"net/http"
+	"strings"
 )
 
-func GetHandler(res http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodGet {
-		http.Error(res, "Only GET requests are allowed!", http.StatusBadRequest)
-		return
+const (
+	addr     = "localhost:8080"
+	idLength = 8
+	charset  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+)
+
+var (
+	urlMap = make(map[string]string)
+)
+
+func generateShortID() string {
+	b := make([]byte, idLength)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
 	}
-	res.Header().Set("Location", "https://practicum.yandex.ru/")
-	res.WriteHeader(http.StatusTemporaryRedirect)
+	return string(b)
 }
 
-func PostHandler(res http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodPost {
-		http.Error(res, "Only POST requests are allowed!", http.StatusBadRequest)
+func shortenURLHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusBadRequest)
 		return
 	}
-	body := req.URL.Path + "id"
-	res.Header().Set("Content-Type", "text/plain")
-	res.WriteHeader(http.StatusCreated)
-	res.Write([]byte(body))
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil || len(body) == 0 {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	originalURL := string(body)
+
+	var shortID string
+	for {
+		shortID = generateShortID()
+		if _, exists := urlMap[shortID]; !exists {
+			urlMap[shortID] = originalURL
+			break
+		}
+	}
+
+	shortURL := "http://localhost:8080/" + shortID
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(shortURL))
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	shortID := strings.TrimPrefix(r.URL.Path, "/")
+	fmt.Println(shortID)
+	if len(shortID) == 0 {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	originalURL, exists := urlMap[shortID]
+
+	if !exists {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+	fmt.Println(originalURL)
+	http.Redirect(w, r, originalURL, http.StatusTemporaryRedirect)
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/id`, GetHandler)
-	mux.HandleFunc(`/`, PostHandler)
-	if err := http.ListenAndServe(`localhost:8080`, mux); err != nil {
-		fmt.Println(err.Error())
+	mux.HandleFunc("/", shortenURLHandler)
+	mux.HandleFunc("/{id}", redirectHandler)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		panic(err)
 	}
 }
