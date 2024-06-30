fea0020824ed7eff380e57250f0ce2e0f79b0122
Tigran <aruttigran1203@yandex.ru>
2024-06-30 15:28:53 +0300

    first prototype

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+func GetHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(res, "Only GET requests are allowed!", http.StatusBadRequest)
+		return
+	}
+	res.Header().Set("Location", "https://practicum.yandex.ru/")
+	res.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func PostHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(res, "Only POST requests are allowed!", http.StatusBadRequest)
+		return
+	}
+	body := req.URL.Path + "id"
+	res.Header().Set("Content-Type", "text/plain")
+	res.WriteHeader(http.StatusCreated)
+	res.Write([]byte(body))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/id`, GetHandler)
+	mux.HandleFunc(`/`, PostHandler)
+	if err := http.ListenAndServe(`localhost:8080`, mux); err != nil {
+		fmt.Println(err.Error())
+	}
+}
