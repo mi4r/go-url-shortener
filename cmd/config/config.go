df2378a2eb8f7f8d8d3b394e3fcf94aa550cd67b
Tigran <aruttigran1203@yandex.ru>
2024-07-06 00:24:59 +0300

    add env in config

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 type Flags struct {
@@ -18,6 +19,14 @@ func Init() *Flags {
 	addr := flag.String("a", "localhost:8080", "Address and port to run server")
 	base := flag.String("b", "http://localhost:8080", "Base shorten url")
 	flag.Parse()
+
+	if envAddr := os.Getenv("SERVER_ADDRESS"); envAddr != "" {
+		*addr = envAddr
+	}
+	if envBase := os.Getenv("BASE_URL"); envBase != "" {
+		*base = envBase
+	}
+
 	return &Flags{
 		RunAddr:       *addr,
 		BaseShortAddr: *base,
