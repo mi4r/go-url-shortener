26bc75aa65e3f90cb99a731a49e72836aa3570d8
Tigran <aruttigran1203@yandex.ru>
2024-07-21 00:30:24 +0300

    add logging

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,9 +5,11 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/mi4r/go-url-shortener.git/cmd/config"
+	"go.uber.org/zap"
 )
 
 const (
@@ -18,7 +20,35 @@ const (
 var (
 	urlMap = make(map[string]string)
 	flags  *config.Flags
+	sugar  zap.SugaredLogger
 )
+
+type (
+	// берём структуру для хранения сведений об ответе
+	responseData struct {
+		status int
+		size   int
+	}
+
+	// добавляем реализацию http.ResponseWriter
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	// записываем ответ, используя оригинальный http.ResponseWriter
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size // захватываем размер
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	// записываем код статуса, используя оригинальный http.ResponseWriter
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode // захватываем код статуса
+}
 
 func generateShortID() string {
 	b := make([]byte, idLength)
@@ -55,7 +85,7 @@ func shortenURLHandler(w http.ResponseWriter, req *http.Request) {
 	_, err = w.Write([]byte(shortURL))
 	if err != nil {
 		http.Error(w, "Failed to write response", http.StatusBadRequest)
-		log.Println("Failed to write response:", err)
+		sugar.Error("Failed to write response", zap.Error(err))
 	}
 }
 
@@ -76,10 +106,46 @@ func redirectHandler(w http.ResponseWriter, req *http.Request) {
 	http.Redirect(w, req, originalURL, http.StatusTemporaryRedirect)
 }
 
+func LoggingMiddleware(h http.Handler) http.Handler {
+	logFn := func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+		lw := loggingResponseWriter{
+			ResponseWriter: w, // встраиваем оригинальный http.ResponseWriter
+			responseData:   responseData,
+		}
+		h.ServeHTTP(&lw, r) // внедряем реализацию http.ResponseWriter
+
+		duration := time.Since(start)
+
+		sugar.Infoln(
+			"uri", r.RequestURI,
+			"method", r.Method,
+			"status", responseData.status, // получаем перехваченный код статуса ответа
+			"duration", duration,
+			"size", responseData.size, // получаем перехваченный размер ответа
+		)
+	}
+	return http.HandlerFunc(logFn)
+}
+
 func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalf("can't initialize zap logger: %v", err)
+	}
+	defer logger.Sync()
+
+	sugar = *logger.Sugar()
+
 	flags = config.Init()
 
 	r := chi.NewRouter()
+	r.Use(LoggingMiddleware)
 	r.Route("/", func(r chi.Router) {
 		r.Post("/", shortenURLHandler)
 		r.Route("/{id}", func(r chi.Router) {
@@ -87,6 +153,6 @@ func main() {
 		})
 	})
 
-	log.Printf("Starting server on %s\n", flags.RunAddr)
+	sugar.Info("Starting server", zap.String("address", flags.RunAddr))
 	log.Fatal(http.ListenAndServe(flags.RunAddr, r))
 }
