d8967235aa56e237c5f3a75549de00cb7cb252f1
Tigran <aruttigran1203@yandex.ru>
2025-02-07 21:27:04 +0300

    back up config file support

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -4,6 +4,7 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -11,16 +12,17 @@ import (
 
 // Flags представляет конфигурационные параметры приложения.
 type Flags struct {
-	RunAddr            string // Адрес и порт для запуска сервера.
-	BaseShortAddr      string // Базовый URL для сокращенных ссылок.
-	URLStorageFilePath string // Путь к файлу для хранения URL (если используется файловое хранилище).
-	DataBaseDSN        string // DSN (Data Source Name) для подключения к базе данных.
-	HTTPSEnabled       bool   // Возможность подключения к HTTPS-серверу
+	RunAddr            string `json:"server_address"`    // Адрес и порт для запуска сервера.
+	BaseShortAddr      string `json:"base_url"`          // Базовый URL для сокращенных ссылок.
+	URLStorageFilePath string `json:"file_storage_path"` // Путь к файлу для хранения URL (если используется файловое хранилище).
+	DataBaseDSN        string `json:"database_dsn"`      // DSN (Data Source Name) для подключения к базе данных.
+	HTTPSEnabled       bool   `json:"enable_https"`      // Возможность подключения к HTTPS-серверу
 }
 
 // String возвращает строковое представление текущих параметров конфигурации.
 func (f *Flags) String() string {
-	return fmt.Sprintf("RunAddr: %s, BaseShortAddr: %s, URLStorageFileName: %s, DataBaseDSN: %s", f.RunAddr, f.BaseShortAddr, f.URLStorageFilePath, f.DataBaseDSN)
+	return fmt.Sprintf("RunAddr: %s, BaseShortAddr: %s, URLStorageFileName: %s, DataBaseDSN: %s, HTTPSEnabled: %t",
+		f.RunAddr, f.BaseShortAddr, f.URLStorageFilePath, f.DataBaseDSN, f.HTTPSEnabled)
 }
 
 // Init инициализирует параметры конфигурации из флагов командной строки, переменных окружения и значений по умолчанию.
@@ -35,6 +37,7 @@ func Init() *Flags {
 	storagePath := flag.String("f", "", "URL storage path")
 	dataBase := flag.String("d", "", "Database connection address")
 	httpsEnabled := flag.Bool("s", false, "Enable HTTPS")
+	configFile := flag.String("c", "", "Path to JSON config file")
 	flag.Parse()
 
 	// Переопределение значений из переменных окружения, если они заданы.
@@ -53,13 +56,43 @@ func Init() *Flags {
 	if envHTTPSEnabled := os.Getenv("ENABLE_HTTPS"); envHTTPSEnabled == "true" {
 		*httpsEnabled = true
 	}
+	if envConfigFile := os.Getenv("CONFIG"); envConfigFile != "" {
+		*configFile = envConfigFile
+	}
 
-	// Возвращает инициализированную структуру Flags.
-	return &Flags{
+	config := Flags{
 		RunAddr:            *addr,
 		BaseShortAddr:      *base,
 		URLStorageFilePath: *storagePath,
 		DataBaseDSN:        *dataBase,
 		HTTPSEnabled:       *httpsEnabled,
 	}
+
+	if *configFile != "" {
+		file, err := os.Open(*configFile)
+		if err == nil {
+			defer file.Close()
+			decoder := json.NewDecoder(file)
+			var fileConfig Flags
+			if err := decoder.Decode(&fileConfig); err == nil {
+				if *addr == "localhost:8080" && fileConfig.RunAddr != "" {
+					config.RunAddr = fileConfig.RunAddr
+				}
+				if *base == "http://localhost:8080" && fileConfig.BaseShortAddr != "" {
+					config.BaseShortAddr = fileConfig.BaseShortAddr
+				}
+				if *storagePath == "" && fileConfig.URLStorageFilePath != "" {
+					config.URLStorageFilePath = fileConfig.URLStorageFilePath
+				}
+				if *dataBase == "" && fileConfig.DataBaseDSN != "" {
+					config.DataBaseDSN = fileConfig.DataBaseDSN
+				}
+				if !*httpsEnabled && fileConfig.HTTPSEnabled {
+					config.HTTPSEnabled = true
+				}
+			}
+		}
+	}
+
+	return &config
 }
